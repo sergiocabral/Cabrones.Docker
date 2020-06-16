197eb33818d10d687e52b8c33782f01bfbd45b8e
SergioCabral <git@sergiocabral.com>
2020-06-16 09:27:59 +0000

    Variável de ambiente PGID para netdata.

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 INITIAL_DIR=$(pwd);
+
 cd docker-compose;
-VIRTUALIZATION=$(systemd-detect-virt -v) docker-compose up $@
+
+VIRTUALIZATION=$(systemd-detect-virt -v) \
+	PGID=$(grep docker /etc/group | cut -d ':' -f 3) \
+	docker-compose up $@
+
 cd $INITIAL_DIR;
