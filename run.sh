50955217abfd4b9c6b4a1120340c17738bcdcfcc
SergioCabral <git@sergiocabral.com>
2020-06-16 09:44:11 +0000

    Removido variável de ambiente PGID para Netdata.

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@ INITIAL_DIR=$(pwd);
 
 cd docker-compose;
 
-VIRTUALIZATION=$(systemd-detect-virt -v) \
-	PGID=$(grep docker /etc/group | cut -d ':' -f 3) \
-	docker-compose up $@
+VIRTUALIZATION=$(systemd-detect-virt -v) docker-compose up $@
 
 cd $INITIAL_DIR;
