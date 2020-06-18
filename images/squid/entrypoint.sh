22ee4187eb8e1b0be40739485cfebe678590892d
SergioCabral <git@sergiocabral.com>
2020-06-18 07:11:39 +0000

    Reescrito imagem squid

--- images/squid/entrypoint.sh
+++ images/squid/entrypoint.sh
@@ -1,96 +1,106 @@
 #!/bin/bash
+
+function create_dir {
+    local VAR=$1;
+    local DIR=${!VAR};
+    printf "Configuring directory: $VAR\n";
+    mkdir -p $DIR;
+    chmod -R 755 $DIR;
+    chown -R squid:squid $DIR;
+    ls -ld $(realpath $DIR);
+}
+
 set -e;
 
-create_conf_template_dir() {
-    mkdir -p ${SQUID_CONF_TEMPLATE_DIR};
-    chmod -R 755 ${SQUID_CONF_TEMPLATE_DIR};
-    chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CONF_TEMPLATE_DIR};
-}
+printf "Entrypoint for docker image: squid\n";
 
-create_conf_final_dir() {
-    mkdir -p ${SQUID_CONF_FINAL_DIR};
-    chmod -R 755 ${SQUID_CONF_FINAL_DIR};
-    chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CONF_FINAL_DIR};
-}
+ARGS="$*";
 
-create_passwd_dir() {
-    mkdir -p ${SQUID_PASSWD_DIR};
-    chmod -R 755 ${SQUID_PASSWD_DIR};
-    chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_PASSWD_DIR};
-}
+SUFFIX_TEMPLATE="template";
 
-create_log_dir() {
-    mkdir -p ${SQUID_LOG_DIR};
-    chmod -R 755 ${SQUID_LOG_DIR};
-    chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR};
-}
+FILE_SQUID=$(which squid || echo "");
+FILE_CONF="squid.conf";
+FILE_CONF_BACKUP="$FILE_CONF.original";
 
-create_cache_dir() {
-    mkdir -p ${SQUID_CACHE_DIR};
-    chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR};
-}
+DIR_SCRIPTS="/root";
+DIR_CONF="/etc/squid";
+DIR_CONF_FINAL="$DIR_CONF/conf";
+DIR_CONF_TEMPLATES="$DIR_CONF/conf.templates";
+DIR_PASSWD="$DIR_CONF/passwd";
+DIR_CACHE="/var/spool/squid";
+DIR_LOG="/var/log/squid";
 
-create_conf_template_dir;
-create_conf_final_dir;
-create_passwd_dir;
-create_log_dir;
-create_cache_dir;
-
-if [ -n "$SQUID_AUTH_USER" ];
+if [ ! -e "$FILE_SQUID" ];
 then
-    printf "Setting user $SQUID_AUTH_USER and password.\n";
-    htpasswd -bc "${SQUID_PASSWD_DIR}/passwd" "$SQUID_AUTH_USER" "$SQUID_AUTH_PASS";    
+    printf "Squid is not installed.\n" >> /dev/stderr;
+    exit 1;
 fi
 
-cd /etc/squid;
-/envsubst.sh;
+IS_FIRST_CONFIGURATION=$((test ! -e $DIR_CONF/$FILE_CONF_BACKUP && echo true) || echo false);
 
-# allow arguments to be passed to squid
-if [[ ${1:0:1} = '-' ]];
+if [ $IS_FIRST_CONFIGURATION = true ];
 then
-
-    EXTRA_ARGS="$@";
-    set --;
-
-elif [[ ${1} == squid || ${1} == $(which squid) ]];
-then
-
-    EXTRA_ARGS="${@:2}";
-    set --;
-
-fi
-
-# default behaviour is to launch squid
-if [[ -z ${1} ]];
-then
-
-    if [[ ! -d ${SQUID_CACHE_DIR}/00 ]];
-    then
-
-        printf "Initializing cache...\n";
-        $(which squid) -N -f /etc/squid/squid.conf -z;
-
-        mv /etc/squid/squid.conf /etc/squid/squid.conf.original;
-
-    fi
-
-    if [[ ! -e "$SQUID_CONF_FINAL_DIR/squid.conf" ]];
-    then
-        cp /etc/squid/squid.conf.original $SQUID_CONF_FINAL_DIR/squid.conf;
-    fi
-
-    if [ -e "/etc/squid/squid.conf" ];
-    then
-        rm /etc/squid/squid.conf;
-    fi
-    
-    ln -s $SQUID_CONF_FINAL_DIR/squid.conf /etc/squid/squid.conf;
-
-    printf "Starting squid...\n";
-    exec $(which squid) -f $SQUID_CONF_FINAL_DIR/squid.conf -NYCd 1 ${EXTRA_ARGS};
-
+    printf "This is the FIRST RUN.\n";
 else
-
-    exec "$@";
-
+    printf "This is NOT the first run.\n";
 fi
+
+create_dir DIR_CONF;
+create_dir DIR_CONF_FINAL;
+create_dir DIR_CONF_TEMPLATES;
+create_dir DIR_PASSWD;
+create_dir DIR_CACHE;
+create_dir DIR_LOG;
+
+printf "Deleting previous authentication data.\n";
+rm -f $DIR_PASSWD/passwd;
+
+if [ -z $SQUID_USERS ];
+then
+    printf "No users was found in environment variable SQUID_USERS.\n";
+else
+    printf "Users data found in environment variable SQUID_USERS.\n";
+
+    touch $DIR_PASSWD/passwd;
+
+    readarray -t AUTH_LIST < <($DIR_SCRIPTS/split-to-lines.sh "," $SQUID_USERS);    
+    for AUTH in ${AUTH_LIST[@]};
+    do
+        readarray -t USER_PASS < <($DIR_SCRIPTS/split-to-lines.sh "=" $AUTH);
+
+        USER=${USER_PASS[0]};
+        PASS=${USER_PASS[1]};
+
+        htpasswd -b $DIR_PASSWD/passwd "$USER" "$PASS";
+    done
+
+    printf "The authentication data was saved.\n";
+    ls -l $DIR_PASSWD/passwd;
+fi
+
+if [ $IS_FIRST_CONFIGURATION = true ];
+then
+    printf "Backing up the configuration file.\n";
+    mv $DIR_CONF/$FILE_CONF $DIR_CONF/$FILE_CONF_BACKUP;
+    ls -l $DIR_CONF/$FILE_CONF_BACKUP;
+fi
+
+if [ ! -e "$DIR_CONF_TEMPLATES/$FILE_CONF.$SUFFIX_TEMPLATE" ];
+then
+    printf "Creating the default template configuration file.\n";
+    cp $DIR_CONF/$FILE_CONF_BACKUP $DIR_CONF_TEMPLATES/$FILE_CONF.$SUFFIX_TEMPLATE;
+    ls -l $DIR_CONF_TEMPLATES/$FILE_CONF.$SUFFIX_TEMPLATE;
+fi
+
+$DIR_SCRIPTS/envsubst-file.sh "$DIR_CONF_TEMPLATES/$FILE_CONF.$SUFFIX_TEMPLATE" "$DIR_CONF_FINAL/$FILE_CONF";
+
+if [ ! -d "${DIR_CACHE}/00" ];
+then
+    printf "Initializing squid cache.\n";
+    $FILE_SQUID -Nz -f $DIR_CONF_FINAL/$FILE_CONF;
+fi
+
+printf "Starting squid...\n";
+
+$FILE_SQUID -f $DIR_CONF_FINAL/$FILE_CONF -NYCd 1 ${ARGS};
+
